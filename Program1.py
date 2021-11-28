f05afb500fdab4383c2acbb0b51d8bd31d7138cc
unknown <you@example.com>
2021-11-28 13:37:18 +0800

    Word counting functional, removed the possible unecessary white space inputs

--- Program1.py
+++ Program1.py
@@ -1,6 +1,7 @@
 # Program 1:
 # Create a program that ask for a sentence as input.
 # Display the number of words, vowels and consonants in the input
+import re
 
 def getWordsNum(char, text):
     count = 1
@@ -9,6 +10,13 @@ def getWordsNum(char, text):
             count += 1
     return count
 
+# user input
 sentence = input("Enter a sentence: ")
+
+# Removing leading, trailing, and multiple whitespaces in the input
+sentence = sentence.strip()
+sentence = re.sub('  +', ' ', sentence)
+
+# getting the number of words
 wordsNum = getWordsNum(" ", sentence)
-print(f"Number of Words: {wordsNum}")
+print(f"Number of words: {wordsNum}")
