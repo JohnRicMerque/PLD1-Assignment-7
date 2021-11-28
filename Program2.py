0684ee461d48bbb52689fe1bb1fda89a0ea21326
unknown <you@example.com>
2021-11-28 23:39:44 +0800

    Program Complete

--- Program2.py
+++ Program2.py
@@ -10,27 +10,27 @@ import string
 
 # looping characters and incrementing variables
 def validatePass(password):
-    length = False
+    letterNum = 0
     capLettersNum = 0
     numbersNum = 0
     specialChar = 0
     for char in password:
-        if len(password) > 15:
-            length = True
+        if char.isalpha():
+            letterNum += 1
         if char.isupper(): 
             capLettersNum += 1
         if char.isnumeric(): 
             numbersNum += 1
         if char in set(string.punctuation): 
             specialChar += 1
-    return length, capLettersNum, numbersNum, specialChar
+    return letterNum, capLettersNum, numbersNum, specialChar
 
 # Asks user input, validates and declares validation
 def askPassValidate():
     userPassword = input("Enter Password: ")
-    lengthG, capG, numG, specialG = validatePass(userPassword)
+    letterG, capG, numG, specialG = validatePass(userPassword)
     # conditions 
-    if lengthG == True and capG > 0 and numG > 0 and specialG > 0:
+    if letterG > 15 and capG > 0 and numG > 0 and specialG > 0:
         print("Password is Valid.")
     else:
         print("Password is Invalid. Please Try again.")
