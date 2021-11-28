f1f781c8af25899da189215d2e752de6d25be51f
unknown <you@example.com>
2021-11-28 14:34:45 +0800

    Added a vowel count function, accepts upper and lower cases

--- Program1.py
+++ Program1.py
@@ -3,20 +3,36 @@
 # Display the number of words, vowels and consonants in the input
 import re
 
-def getWordsNum(char, text):
+# functions
+def getWordsNum(spaces, text):
     count = 1
     for i in text:
-        if char == i:
+        if spaces == i:
             count += 1
     return count
+
+def getVowelsNum(vowels, text):
+    text = text.lower()
+    count = 0
+    for i in text:
+        if i in vowels:
+            count += 1
+    return count
+
+# lists
+vowelsList = ["a", "e", "i", "o", "u"]
 
 # user input
 sentence = input("Enter a sentence: ")
 
-# Removing leading, trailing, and multiple whitespaces in the input
+# removing leading, trailing, and multiple whitespaces in the input
 sentence = sentence.strip()
 sentence = re.sub('  +', ' ', sentence)
 
 # getting the number of words
 wordsNum = getWordsNum(" ", sentence)
 print(f"Number of words: {wordsNum}")
+
+# getting the number of vowels
+vowelsNum = getVowelsNum(vowelsList, sentence)
+print(f"Number of vowels: {vowelsNum}")
