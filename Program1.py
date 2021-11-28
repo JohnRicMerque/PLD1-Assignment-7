f7251afa54a3ee81efaebb5cb22217bffabbbf36
unknown <you@example.com>
2021-11-28 15:26:09 +0800

    Added a consonant count function, accepts upper and lower cases

--- Program1.py
+++ Program1.py
@@ -1,6 +1,7 @@
 # Program 1:
 # Create a program that ask for a sentence as input.
 # Display the number of words, vowels and consonants in the input
+
 import re
 
 # functions
@@ -19,9 +20,20 @@ def getVowelsNum(vowels, text):
             count += 1
     return count
 
+def getConsonantsNum(consonants, text):
+    text = text.lower()
+    count = 0
+    for i in text:
+        if i in consonants:
+            count += 1
+    return count
+
 # lists
 vowelsList = ["a", "e", "i", "o", "u"]
-
+consonantsList = [
+    "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w","x", "y", "z"
+    ]
+    
 # user input
 sentence = input("Enter a sentence: ")
 
@@ -36,3 +48,7 @@ print(f"Number of words: {wordsNum}")
 # getting the number of vowels
 vowelsNum = getVowelsNum(vowelsList, sentence)
 print(f"Number of vowels: {vowelsNum}")
+
+# getting the number of consonants
+consonantsNum = getConsonantsNum(consonantsList, sentence)
+print(f"Number of consonants: {consonantsNum}")
