88ad260aed1d64e649ca2945c00c61d7b67a776c
unknown <you@example.com>
2021-12-02 22:55:26 +0800

    Changed condition from 15 letters to 15 characters

--- Program2.py
+++ Program2.py
@@ -1,7 +1,7 @@
 # Program 2: Password validator
 # Create a program that check if password is valid
 # The password is valid if all criteria are met:
-# a. Greater than 15 letters
+# a. Greater than 15 characters
 # b. Have at least one capital letter
 # c. Have at least one number
 # d. Have at least one special char
@@ -10,38 +10,38 @@ import string
 
 # looping characters and incrementing variables
 def validatePass(password):
-    letterNum = 0
+    char15 = False
     capLettersNum = 0
     numbersNum = 0
     specialChar = 0
     for char in password:
-        if char.isalpha():
-            letterNum += 1
+        if len(password) > 15:
+            char15 = True
         if char.isupper(): 
             capLettersNum += 1
         if char.isnumeric(): 
             numbersNum += 1
         if char in set(string.punctuation): 
             specialChar += 1
-    return letterNum, capLettersNum, numbersNum, specialChar
+    return char15, capLettersNum, numbersNum, specialChar
 
-# Asks user input, validates and declares validation
+# Asks user input, validates and declares if valid or invalid
 def askPassValidate():
     while True:
         userPassword = input("Enter Password: ")
-        letterG, capG, numG, specialG = validatePass(userPassword)
+        charG, capG, numG, specialG = validatePass(userPassword)
         # conditions 
-        if letterG > 15 and capG > 0 and numG > 0 and specialG > 0:
+        if charG == True and capG > 0 and numG > 0 and specialG > 0:
             print("Password is Valid.")
             break
-        if letterG <= 15:
-            print("Invalid. Please include greater than 15 letters")
+        if charG == False:
+            print("Invalid. Please include greater than 15 characters.")
         if capG == 0:
-            print("Invalid. Please include at least one capital letter")
+            print("Invalid. Please include at least one capital letter.")
         if numG == 0:
-            print("Invalid. Please include at least one number")
+            print("Invalid. Please include at least one number.")
         if specialG == 0:
-            print("Invalid. Please include at least one special character")
+            print("Invalid. Please include at least one special character.")
         continue
     
 # declaring the function
